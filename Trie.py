425e71faf299528c78e7dd960057dee14388a4a4
Evan Mikesell <mikesellevan@gmail.com>
2022-10-10 00:07:19 -0700

    Update Trie.py

--- Trie.py
+++ Trie.py
@@ -1,52 +1,58 @@
 class Trie:
 
     def __init__(self):
-        self.root = self.Node('', False)
+        self._root = self.Node('')
 
-    def insert(self, word: str):
-        current_node = self.root
-        for char in word:
-            if char not in current_node.children:
-                current_node.children[char] = self.Node(char, False)
+    def update(self, word: str):
+        """Each letter in a word becomes a child of the previous letter."""
+        current_node = self._root
+        for letter in word:
+            if letter not in current_node._children:
+                current_node._children[letter] = self.Node(letter)
+            current_node = current_node._children[letter]
+        current_node._is_end_of_word = True
 
-            current_node = current_node.children[char]
+    def words(self, substring: str = None) -> list:
+        """
+        Gets a list of all words in the trie.
+        If a substring is specified, it will only include words with the substring.
+        """
+        all_words = self._root._find_all_words(self._root)
+        if not substring:
+            return all_words
+        else:
+            words_containing_substring = []
+            for word in all_words:
+                if substring in word:
+                    words_containing_substring.append(word)
+            return words_containing_substring
 
-        # last letter of the word is the end of the word
-        current_node.is_end_of_word = True
-
-    def find_words(self, substring='') -> list:
-        return self.root.find_words('', self.root, substring)
+    def contains(self, word: str) -> bool:
+        all_words = self._root._find_all_words(self._root)
+        if word in all_words:
+            return True
+        else:
+            return False
 
     class Node:
-        """def __init__(self):
-            self.letter = ''
-            self.isEndofWord = False
-            self.children = dict()"""
 
-        def __init__(self, letter: str, is_end_of_word: bool):
-            self.letter = letter
-            self.is_end_of_word = is_end_of_word
-            self.children = dict()
+        def __init__(self, letter: str):
+            self._letter = letter
+            self._is_end_of_word = False
+            self._children = dict()
 
-        def find_words(self, word: str, current_node: 'Node', substring: str = '', word_list: list = []) -> list:
-            word += current_node.letter
-            if current_node.is_end_of_word:
-                if substring in word:
-                    word_list.append(word)
-            for child in current_node.children.values():
-                self.find_words(word, child, substring, word_list)
-            return word_list
-
-
-tree = Trie()
-tree.insert('dog')
-tree.insert('log')
-tree.insert('day')
-tree.insert('hello')
-tree.insert('hi')
-
-ar_words = tree.find_words(substring='ar')
-print("Words with 'ar'", ar_words)
-
-all_words = tree.find_words()
-print("All words", all_words)
+        def _find_all_words(self, current_node: 'Node', words: list = None,
+                            current_word: str = '') -> list:
+            """
+            Moves through the tree building up words based on each node's letter.
+            Returns a list of all words present in the trie.
+            """
+            if words is None:
+                words = []
+            current_word += current_node._letter
+            if current_node._is_end_of_word:
+                words.append(current_word)
+            if current_node._children:
+                for child in current_node._children.values():
+                    self._find_all_words(child, words, current_word)
+            return words
